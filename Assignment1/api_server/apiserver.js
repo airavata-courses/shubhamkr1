2df1c237b90234c4ec03e0550fdc1b41a818c8a3
shubhamkr1 <shubhamkumar007@gmail.com>
2017-09-11 14:06:15 -0400

    #9 fixed index.html and apiserver.js, added server for UI

--- Assignment1/api_server/apiserver.js
+++ Assignment1/api_server/apiserver.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var request = require('request');
 
 var app = express();
 
+// CORS
+// This allows client applications from other domains use the API Server
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname+'/static'));
 app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 20000);
@@ -16,21 +23,22 @@ app.get('/', function(req, res){
 	res.send('');
 });
 
-//node service
-app.get('http://localhost:3000/', function(req,res){
-	res.send('');
-});
 
-//python microservice
-app.get('http://localhost:5000/', function(req,res){
-	res.send('');
-});
+// calling node microservices
+var add_item= function (req, res) {
 
-//java spring microservice
-app.get('http://localhost:8080/', function(req,res){
+  request("http://localhost:3000/nodejs", function(error, response, body) {
+    if (error) console.log(error);
+    else {
+      console.log(response.body);
+      res.send(response.body);
+    }
+    
+  });
+ };
 
-});
 
+app.get('/apigateway/',[add_item]);
 
 app.listen(app.get('port'),function() {
 	console.log('Server  listening on port 20000!')
